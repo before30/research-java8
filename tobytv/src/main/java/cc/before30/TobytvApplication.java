074e3598a28b798172ffefc4b938850d048e2502
before30 <before30@gmail.com>
2016-11-15 00:24:58 +0900

    Modify Super Type Token

---
+++ tobytv/src/main/java/cc/before30/TobytvApplication.java
@@ -0,0 +1,12 @@
+package cc.before30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobytvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobytvApplication.class, args);
+	}
+}
