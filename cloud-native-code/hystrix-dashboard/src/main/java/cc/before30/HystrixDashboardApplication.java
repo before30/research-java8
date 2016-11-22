d43620692deafc480459b95f0a5ab673ae98d974
before30 <before30@gmail.com>
2016-11-22 20:16:15 +0900

    Add workshop projects

---
+++ cloud-native-code/hystrix-dashboard/src/main/java/cc/before30/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package cc.before30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+}
