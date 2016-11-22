9dce14d7e44b80fbfe6f45cbccfece0f8aafa02a
before30 <before30@gmail.com>
2016-11-22 12:37:57 +0900

    Introduce cloud config server

---
+++ cloud-native-code/config-server/src/main/java/cc/before30/ConfigServiceApplication.java
@@ -0,0 +1,14 @@
+package cc.before30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+}
