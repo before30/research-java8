2acd1d7e7e94d89bf19192d550faffeccbd0acb7
before30 <before30@gmail.com>
2016-12-12 00:02:44 +0900

    Add CombineLatest Examples

--- tobytv/src/main/java/cc/before30/review/FluxExamples.java
+++ tobytv/src/main/java/cc/before30/review/FluxExamples.java
@@ -1,5 +1,6 @@
 package cc.before30.review;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 import reactor.util.function.Tuple3;
@@ -11,33 +12,51 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by before30 on 11/12/2016.
  */
+@Slf4j
 public class FluxExamples {
     public static void main(String[] args) throws InterruptedException {
-        final Flux<Long> src1 = Flux.fromIterable(Iter::new)
-                .publishOn(Schedulers.newSingle("src1"));
+        //        Flux.range(1, 10)
+//                .log()
+//                .subscribe(System.out::println);
 
-        final Flux<Long> src2 = Flux.fromIterable(Iter::new)
-                .publishOn(Schedulers.newSingle("src2"));
-
-        Flux.combineLatest(
-                v-> Tuples.of((long)v[0],(long)v[1]), 1,
-                src1, src2)
+        Flux.range(1, 10)
                 .log()
-                .take(10);
+                .subscribe(System.out::println);
 
+        final Flux<Long> numSrc = Flux.fromIterable(IterNum::new)
+                                        .publishOn(Schedulers.newSingle("numSrc"));
+        final Flux<Character> charSrc = Flux.fromIterable(IterChar::new)
+                                            .publishOn(Schedulers.newSingle("charSrc"));
 
-        Tuple3<Integer, Integer, Integer> tuples = Tuples.of(1, 2, 3);
-        System.out.println(tuples);
+        Flux.combineLatest(numSrc, charSrc, (n, c) -> Tuples.of(n, c))
+                .log()
+                .subscribe(System.out::println);
 
-        System.out.println(tuples.getT1());
-        System.out.println(tuples.getT2());
-        System.out.println(tuples.getT3());
-
-        TimeUnit.SECONDS.sleep(10);
+//        TimeUnit.SECONDS.sleep(10);
     }
 }
 
-class Iter implements Iterator<Long> {
+class IterChar implements Iterator<Character> {
+    char charactor = 'a';
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public Character next() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        charactor = (char)((++charactor) % ('z' - 'a') + 'a');
+        return charactor;
+    }
+}
+
+class IterNum implements Iterator<Long> {
     long count = 0;
 
     @Override
