df8a8835b4447fb26cf852da9c1d8184efbffe57
before30 <before30@gmail.com>
2016-12-16 21:43:14 +0900

    Introduce another examples of method reference

--- mdjava/src/main/java/cc/before30/modernjava/ep11/MethodReferenceEx.java
+++ mdjava/src/main/java/cc/before30/modernjava/ep11/MethodReferenceEx.java
@@ -1,6 +1,9 @@
 package cc.before30.modernjava.ep11;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 /**
  * Created by before30 on 16/12/2016.
@@ -14,5 +17,29 @@ public class MethodReferenceEx {
 
         // Method Reference
 
+        System.out.println(
+        Arrays.asList(new BigDecimal("10.0"), new BigDecimal("23"), new BigDecimal("5"))
+                .stream()
+//                .sorted(Comparator.naturalOrder())
+//                .sorted((bd1, bd2) -> bd2.compareTo(bd2))
+                .sorted(BigDecimal::compareTo)
+                .collect(Collectors.toList()));
+
+
+
+        final String targetString = "c";
+        System.out.println(
+        Arrays.asList("a", "b", "c", "d")
+                .stream()
+//                .anyMatch(x -> x.equals("c")));
+//                .anyMatch(targetString::equals)
+                .anyMatch("c"::equals)
+        );
+    }
+}
+
+class BigDecimalUtil {
+    public static int compare(BigDecimal bd1, BigDecimal bd2) {
+        return bd1.compareTo(bd2);
     }
 }
