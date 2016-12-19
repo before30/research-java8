4554358372d74a6e94495db0a05e022a8be2eff6
before30 <before30@gmail.com>
2016-12-19 22:21:13 +0900

    Introduce MethodReference Examples

---
+++ mdjava/src/main/java/cc/before30/modernjava/ep11/MethodReferenceEx3.java
@@ -0,0 +1,72 @@
+package cc.before30.modernjava.ep11;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * Created by before30 on 19/12/2016.
+ */
+public class MethodReferenceEx3 {
+    public static void main(String[] args) {
+        methodReference03();
+
+    }
+
+
+    private static void methodReference03() {
+        /* First Class Function */
+        // Function can be passed as a parameter to another function.
+        // Using Lambda Expression
+        System.out.println(testFirstClassFunction1(3, i -> String.valueOf(i * 2)));
+
+        // Using Method Reference
+        System.out.println(testFirstClassFunction1(3, MethodReferenceEx3::doubleThenToString));
+
+        /*
+        Function can be returned as the result of another function
+        Using Lambda Expression
+         */
+        final Function<Integer, String> f1 = getDoubleThenThenToStringUsingLambdaExpression();
+        final String resultFromF1 = f1.apply(3);
+        System.out.println(resultFromF1);
+
+        /*
+        Using Method Reference
+         */
+        final Function<Integer, String> fmr = getDoubleThenToStringUsingMethodReference();
+        final String resultFromFMR = fmr.apply(3);
+        System.out.println(resultFromFMR);
+
+        /*
+        Function can be stored in the data structure.
+         */
+        // Using Lambda Expression
+        final List<Function<Integer, String>> fsL = Arrays.asList(i -> String.valueOf(i * 2));
+        for (final Function<Integer, String> f : fsL) {
+            System.out.println(f.apply(3));
+        }
+
+        // Using Method Reference
+        final List<Function<Integer, String>> fmrL = Arrays.asList(MethodReferenceEx3::doubleThenToString);
+        for (final Function<Integer, String> f : fmrL) {
+            System.out.println(f.apply(3));
+        }
+    }
+
+    private static Function<Integer, String> getDoubleThenToStringUsingMethodReference() {
+        return MethodReferenceEx3::doubleThenToString;
+    }
+
+    private static String doubleThenToString(int i) {
+        return String.valueOf(i * 2);
+    }
+
+    private static String testFirstClassFunction1(int n, Function<Integer, String> f) {
+        return "The result is " + f.apply(n);
+    }
+
+    private static Function<Integer, String> getDoubleThenThenToStringUsingLambdaExpression() {
+        return i -> String.valueOf(i * 2);
+    }
+}
