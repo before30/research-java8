c14976b58fdb539845d6d8e5b9b64b201b490f74
before30 <before30@gmail.com>
2017-01-09 01:06:34 +0900

    Refactoring 3rd step

--- tobytv/src/main/java/cc/before30/review/week10/MyWeek10App.java
+++ tobytv/src/main/java/cc/before30/review/week10/MyWeek10App.java
@@ -46,24 +46,37 @@ public class MyWeek10App {
                     .from(restTemplate.getForEntity(URL_1, String.class, "h" + idx))
                     .andApply(s -> restTemplate.getForEntity(URL_2, String.class, s.getBody()))
                     .andAccept(s -> result.setResult(s.getBody()));
-//            ListenableFuture<ResponseEntity<String>> f1 = restTemplate.getForEntity(URL_1, String.class, idx);
-//            f1.addCallback(s1 -> {
-//                ListenableFuture<ResponseEntity<String>> f2 = restTemplate.getForEntity(URL_2, String.class, s1.getBody());
-//                f2.addCallback(s2 -> {
-//                    ListenableFuture<String> f3 = myService.work(s2.getBody());
-//                    f3.addCallback(s3 -> {
-//                        result.setResult(s3);
-//                    }, e3 -> {
-//                        result.setErrorResult(e3);
-//                    });
-//                }, e2 -> {
-//                    result.setErrorResult(e2);
-//                });
-//            }, e1 -> {
-//                result.setErrorResult(e1);
-//            });
 
             return result;
+        }
+    }
+
+    public static class AcceptMyCompletion extends MyCompletion {
+        Consumer<ResponseEntity<String>> consumer;
+        public AcceptMyCompletion(Consumer<ResponseEntity<String>> consumer) {
+            this.consumer = consumer;
+        }
+
+        @Override
+        void run(ResponseEntity<String> value) {
+            consumer.accept(value);
+        }
+    }
+
+    public static class ApplyMyCompletion extends MyCompletion {
+        Function<ResponseEntity<String>, ListenableFuture<ResponseEntity<String>>> fn;
+        public ApplyMyCompletion(Function<ResponseEntity<String>, ListenableFuture<ResponseEntity<String>>> fn) {
+            this.fn = fn;
+        }
+
+        @Override
+        void run(ResponseEntity<String> value) {
+            ListenableFuture<ResponseEntity<String>> lf = fn.apply(value);
+            lf.addCallback(s -> {
+                complete(s);
+            }, e -> {
+                error(e);
+            });
         }
     }
 
@@ -74,25 +87,17 @@ public class MyWeek10App {
 
         }
 
-        Consumer<ResponseEntity<String>> consumer;
-        public MyCompletion(Consumer<ResponseEntity<String>> consumer) {
-            this.consumer = consumer;
-        }
 
-        Function<ResponseEntity<String>, ListenableFuture<ResponseEntity<String>>> fn;
-        public MyCompletion(Function<ResponseEntity<String>, ListenableFuture<ResponseEntity<String>>> fn) {
-            this.fn = fn;
-        }
 
         public MyCompletion andApply(Function<ResponseEntity<String>, ListenableFuture<ResponseEntity<String>>> fn) {
-            MyCompletion c = new MyCompletion(fn);
+            MyCompletion c = new ApplyMyCompletion(fn);
             this.next = c;
 
             return c;
         }
 
         public void andAccept(Consumer<ResponseEntity<String>> consumer) {
-            MyCompletion c = new MyCompletion(consumer);
+            MyCompletion c = new AcceptMyCompletion(consumer);
             next = c;
         }
 
@@ -114,15 +119,6 @@ public class MyWeek10App {
         }
 
         void run(ResponseEntity<String> value) {
-            if (consumer != null) consumer.accept(value);
-            else if (fn != null) {
-                ListenableFuture<ResponseEntity<String>> lf = fn.apply(value);
-                lf.addCallback(s -> {
-                    complete(s);
-                }, e -> {
-                    error(e);
-                });
-            }
         }
     }
 
