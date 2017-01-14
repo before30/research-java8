868b908c5c377727fce04b147ba1f2eafa0eae89
before30 <before30@gmail.com>
2017-01-15 00:48:48 +0900

    add deferred result

--- nonblocking-ex/src/main/java/cc/before30/contorller/MyController.java
+++ nonblocking-ex/src/main/java/cc/before30/contorller/MyController.java
@@ -3,6 +3,7 @@ package cc.before30.contorller;
 import com.sun.xml.internal.ws.util.CompletedFuture;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskDecorator;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -12,9 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.context.request.async.DeferredResult;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
+import java.util.Queue;
+import java.util.concurrent.*;
 
 /**
  * Created by before30 on 13/01/2017.
@@ -48,19 +48,60 @@ public class MyController {
         te.setMaxPoolSize(100);
         te.setQueueCapacity(200);
         te.setThreadNamePrefix("mythreadpool");
+        te.setTaskDecorator(new TaskDecorator() {
+            @Override
+            public Runnable decorate(Runnable runnable) {
+
+                return () -> {
+                    log.info("before run");
+                    runnable.run();
+                    log.info("after run");
+                };
+            }
+        });
         te.initialize();
         return te;
     }
 
-    @GetMapping("/rest")
-    @Async(value = "tp")
-    public CompletableFuture<String> rest(String idx) throws InterruptedException {
-//        CompletableFuture<String> future = new CompletableFuture<>();
+//    @GetMapping("/rest")
+//    @Async(value = "tp")
+//    public CompletableFuture<String> rest(String idx) throws InterruptedException {
+////        CompletableFuture<String> future = new CompletableFuture<>();
+//
+//        log.info("request");
+//        TimeUnit.SECONDS.sleep(2);
+//        log.info("request...");
+//        return CompletableFuture.completedFuture(idx + "/rest");
+//    }
 
-        log.info("request");
-        TimeUnit.SECONDS.sleep(2);
-        log.info("request...");
-        return CompletableFuture.completedFuture(idx + "/rest");
+//    @GetMapping("/rest")
+//    public Callable<String> rest(String idx) {
+//        log.info("cbefore callable");
+//        return new Callable<String>() {
+//            @Override
+//            public String call() throws Exception {
+//                log.info("async");
+//                TimeUnit.SECONDS.sleep(2);
+//                return idx + "/rest";
+//            }
+//        };
+//    }
+
+    @GetMapping("/rest")
+    public DeferredResult<String> rest(String idx) {
+        log.info("deferred result");
+        DeferredResult<String> dr = new DeferredResult<>();
+
+        Executors.newSingleThreadExecutor().submit(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            dr.setResult(idx + "/rest");
+        });
+
+        return dr;
     }
 
 //    @GetMapping("/rest")
